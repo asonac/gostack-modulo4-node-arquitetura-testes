edf9849b78cc0c95ffea6058b34b746361816da9
Asonac <wesley.canosa@gmail.com>
2020-06-01 08:31:37 -0300

    removed comments

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -19,15 +19,4 @@ routes.get('/', (request, response) =>
   response.json({ message: 'Hello GoStack!' }),
 );
 
-// routes.post('/users', (request, response) => {
-//   const { name, email } = request.body;
-
-//   const user = {
-//     name,
-//     email,
-//   };
-
-//   response.json(user);
-// });
-
 export default routes;
